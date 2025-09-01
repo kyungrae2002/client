50d8a21802c858a0ca2ab9fba97d3f5ee226b0c0
8t-truck <kwoncar3@gmail.com>
2025-09-01 12:49:31 +0900

    design bg-color

--- src/app/composer-talk-room/[id]/layout.tsx
+++ src/app/composer-talk-room/[id]/layout.tsx
@@ -4,10 +4,11 @@ import Link from 'next/link';
 
 export default function ComposerTalkLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="min-h-screen bg-[#f4f5f7] flex flex-col items-center py-8">
+        <div className="min-h-screen bg-white flex flex-col items-center py-8">
             {/* 상단 타이틀 */}
             <header className="w-[375px] flex items-center px-5 mb-6">
-                <Link href="/">
+                <Link href="/composer-talk">
+                {/* href="/composer-talk/작곡가 id"로 경로를 바꿔야함 */}
                     <Image src="/icons/back.svg" alt="뒤로가기" width={24} height={24} />
                 </Link>
                 <div className="flex-grow text-left text-zinc-900 text-base font-semibold font-['Pretendard']">
